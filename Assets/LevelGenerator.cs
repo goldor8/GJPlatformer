a56689129346b1952e9666a3dd1fd9d1c8964bf7
goldor <dimgoldoritri@gmail.com>
2025-04-16 11:01:59 +0200

    save scene

--- Assets/LevelGenerator.cs
+++ Assets/LevelGenerator.cs
@@ -8,18 +8,29 @@ public class LevelGenerator : MonoBehaviour
     public List<LevelPart> levelParts;
     public int length = 10;
 
-    public void Generate(int lenght)
+    public int[] Generate(int lenght)
     {
-        float offset = 0;
         Random random = new Random((uint) DateTime.UtcNow.Second);
+        int[] levelPartIndexes = new int[lenght];
         
         for (int i = 0; i < lenght; i++)
         {
             int randomIndex = random.NextInt(0, levelParts.Count);
-            LevelPart part = levelParts[randomIndex];
-            offset += part.width/2;
-            GameObject partInstance = Instantiate(part.gameObject, new Vector3(offset, 0, 0), Quaternion.identity);
-            offset += part.width/2;
+            levelPartIndexes[i] = randomIndex;
+        }
+
+        return levelPartIndexes;
+    }
+
+    void SpawnLevel(Vector3 offset, int[] parts)
+    {
+        float hOffset = 0;
+        for (int i = 0; i < parts.Length; i++)
+        {
+            LevelPart part = levelParts[parts[i]];
+            hOffset += part.width/2;
+            GameObject partInstance = Instantiate(part.gameObject, new Vector3(hOffset, 0, 0) + offset, Quaternion.identity);
+            hOffset += part.width/2;
             
             // Set the parent of the instantiated part to the generator
             partInstance.transform.SetParent(transform);
